fb78eccb7101d20903535d5a9353dec1f733c57d
Thiago França <thiagofranca.pe1999@gmail.com>
2024-04-21 19:52:45 -0300

    Configurando serviço

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Course} from "../model/course";
+import {CoursesService} from "../services/courses.service";
 
 @Component({
   selector: 'app-courses',
@@ -8,22 +9,16 @@ import {Course} from "../model/course";
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [
-      {_id: '1', name: 'Course 1', category: 'Category 1'},
-      {_id: '2', name: 'Course 2', category: 'Category 2'},
-      {_id: '3', name: 'Course 3', category: 'Category 3'},
-      {_id: '4', name: 'Course 4', category: 'Category 1'},
-      {_id: '5', name: 'Course 5', category: 'Category 2'},
-      {_id: '6', name: 'Course 6', category: 'Category 3'},
-      {_id: '7', name: 'Course 7', category: 'Category 1'},
-      {_id: '8', name: 'Course 8', category: 'Category 2'},
-      {_id: '9', name: 'Course 9', category: 'Category 3'},
-      {_id: '10', name: 'Course 10', category: 'Category 1'}
-      ];
+  courses: Course[] = [];
   displayedColumns = [ 'name', 'category' ];
 
-  constructor() {
+  //coursesService: CoursesService;
+
+  constructor(private coursesService: CoursesService) {
     // this.courses = [];
+    //this.coursesService = new CoursesService();
+
+    this.courses = this.coursesService.list();
   }
 
   ngOnInit(): void {
--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  list(){
+    return [
+      {_id: '1', name: 'Course 1', category: 'Category 1'},
+      {_id: '2', name: 'Course 2', category: 'Category 2'},
+      {_id: '3', name: 'Course 3', category: 'Category 3'},
+      {_id: '4', name: 'Course 4', category: 'Category 4'},
+      {_id: '5', name: 'Course 5', category: 'Category 5'}
+    ]
+  }
 }
